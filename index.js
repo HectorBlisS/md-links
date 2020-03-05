16d1778a39539ecefdb78ca6bc57e6a8f8356bbc
hectorbliss <contacto@fixter.org>
2020-03-05 11:17:39 -0600

    getting back the tryCatch middleware

--- index.js
+++ index.js
@@ -6,41 +6,45 @@ const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 const questions = require("./questions");
+
+function tryCatch(func) {
+  try {
+    func();
+  } catch (e) {
+    console.log(e);
+    throw new Error(e);
+  }
+}
+
 function realFunction(uri, res, rej) {
   return async () => {
-    try {
-      let links = [];
-      let data = fs.readFileSync(uri, "utf8");
-      let output = data.replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/gm, x => {
-        if (x === "#") return;
-        let obj = {
-          href: x
-            .replace(/\(/g, "")
-            .replace(/\)/g, "")
-            .split("]")[1]
-            .replace(/'/g, "")
-            .replace(/"/g, ""),
-          text: x
-            .replace(/\[/g, "")
-            .replace(/\]/g, "")
-            .split("(")[0]
-            .replace(/'/g, "")
-            .replace(/"/g, ""),
-          file: uri
-        };
-        links.push(obj);
-      });
-      console.log("Links: ", links);
-      res(links);
-    } catch (e) {
-      console.log("Links: ", e);
-      rej(e);
-    }
+    let links = [];
+    let data = fs.readFileSync(uri, "utf8");
+    let output = data.replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/gm, x => {
+      if (x === "#") return;
+      let obj = {
+        href: x
+          .replace(/\(/g, "")
+          .replace(/\)/g, "")
+          .split("]")[1]
+          .replace(/'/g, "")
+          .replace(/"/g, ""),
+        text: x
+          .replace(/\[/g, "")
+          .replace(/\]/g, "")
+          .split("(")[0]
+          .replace(/'/g, "")
+          .replace(/"/g, ""),
+        file: uri
+      };
+      links.push(obj);
+    });
+    console.log("Links: ", links);
   };
 }
 
 let mdLinks = uri => {
-  return new Promise((res, rej) => realFunction(uri, res, rej));
+  return new Promise((res, rej) => tryCatch(realFunction(uri, res, rej)));
 };
 
 module.exports = mdLinks;
