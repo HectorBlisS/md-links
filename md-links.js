0cd869e96f563090f1ede52ffd4adc6e48363b0c
hectorbliss <contacto@fixter.org>
2020-05-08 01:18:35 -0500

    try catch added to md-links.js

--- md-links.js
+++ md-links.js
@@ -11,29 +11,34 @@ function tryCatch(func) {
 
 function realFunction(uri, res, rej) {
   return async () => {
-    let links = [];
-    let data = fs.readFileSync(uri, "utf8");
-    let output = data.replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/gm, x => {
-      if (x === "#") return;
-      let obj = {
-        href: x
-          .replace(/\(/g, "")
-          .replace(/\)/g, "")
-          .split("]")[1]
-          .replace(/'/g, "")
-          .replace(/"/g, ""),
-        text: x
-          .replace(/\[/g, "")
-          .replace(/\]/g, "")
-          .split("(")[0]
-          .replace(/'/g, "")
-          .replace(/"/g, ""),
-        file: uri
-      };
-      links.push(obj);
-    });
-    // console.log("Links: ", links);
-    return res(links);
+    try{
+      let links = [];
+      let data = fs.readFileSync(uri, "utf8");
+      let output = data.replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/gm, x => {
+        if (x === "#") return;
+        let obj = {
+          href: x
+            .replace(/\(/g, "")
+            .replace(/\)/g, "")
+            .split("]")[1]
+            .replace(/'/g, "")
+            .replace(/"/g, ""),
+          text: x
+            .replace(/\[/g, "")
+            .replace(/\]/g, "")
+            .split("(")[0]
+            .replace(/'/g, "")
+            .replace(/"/g, ""),
+          file: uri
+        };
+        links.push(obj);
+      });
+      // console.log("Links: ", links);
+      return res(links);
+    }catch(e){
+      return rej(e)
+    }
+
   };
 }
 
